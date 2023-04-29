6d47c2e87369a51cb6177e068f74a7788b232be1
小仔 <chris58530@gmail.com>
2023-04-29 18:15:25 +0800

    EnumTypes Scripts

--- Assets/_Scripts/MagnetBody.cs
+++ Assets/_Scripts/MagnetBody.cs
@@ -122,4 +122,6 @@ public class MagnetBody : MonoBehaviour
             return;
         }
     }
+
+ 
 }
---
+++ Assets/_Scripts/Tools/EnumTypes.cs
@@ -0,0 +1,6 @@
+public enum MagnetType{
+    ATTARCT,REPEL
+}
+public enum SceneType{
+    INTRO,BEGINNER,CONNECT,LOBBY,GAME
+}
