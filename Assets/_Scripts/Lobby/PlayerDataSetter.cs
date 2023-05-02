ce242869bf4190d6cc5a999fb42866530cdfd8ab
小仔 <chris58530@gmail.com>
2023-05-03 00:08:38 +0800

    05/02 更新:
    增加Lobby內選角功能

---
+++ Assets/_Scripts/Lobby/PlayerDataSetter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDataSetter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Model = null;
+
+
+    private void Start()
+    {
+
+
+        // tankSpriteRenderer.color = GameManager.Instance.TankColor;
+        // barrelSpriteRenderer.color = GameManager.Instance.BarrelColor;
+    }
+
+    public void OnSelcectCharacterChanged(GameObject obj)
+    {
+        // GameManager.Instance.TankColor.r = value / 255f;
+        // tankSpriteRenderer.color = GameManager.Instance.TankColor;
+
+        // GameManager.Instance.SetPlayerNetworkData();
+        for (int i = 0; i < Model.Length; i++)
+        {
+            Model[i].SetActive(false);
+            if(Model[i].name == obj.name){
+                Model[i].SetActive(true);
+            }
+        }
+
+    }
+
+
+    public void OnPlayerNameInputFieldChange(string value)
+    {
+        GameManager.Instance.PlayerName = value;
+        GameManager.Instance.SetPlayerNetworkData();
+    }
+}
