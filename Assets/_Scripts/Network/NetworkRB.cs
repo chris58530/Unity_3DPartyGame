37ec96cedef698aa7bed88699603f08255ad5df2
小仔 <chris58530@gmail.com>
2023-05-28 15:34:26 +0800

    測試

--- Assets/_Scripts/Network/NetworkRB.cs
+++ Assets/_Scripts/Network/NetworkRB.cs
@@ -1,26 +1,26 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
 
-// public class NetworkRB : NetworkBehaviour
-// {
-//     Rigidbody rb;
+public class NetworkRB : NetworkBehaviour
+{
+    Rigidbody rb;
     
-//     private void Awake() {
-//         rb = GetComponent<Rigidbody>();
-//     }
-//     public override void FixedUpdateNetwork()
-//     {
-//         if (GetInput(out NetworkInputData inputData))
-//         {
-//             Vector3 moveDir = new Vector3(inputData.AxisX,0,inputData.AxisZ);
-//             if (inputData.Move)
-//             {
-//                 var rotation = Quaternion.LookRotation(moveDir);
-//                 transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 8);
-//             }
-//             rb.AddForce(moveDir * 100);
-//         }
-//     }
-// }
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData inputData))
+        {
+            Vector3 moveDir = new Vector3(inputData.AxisX,0,inputData.AxisZ);
+            if (inputData.Move)
+            {
+                var rotation = Quaternion.LookRotation(moveDir);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 8);
+            }
+            rb.AddForce(moveDir * 100);
+        }
+    }
+}
