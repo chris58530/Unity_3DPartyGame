14d1be324aeb826207e3ad314c8a9744378612f4
小仔 <chris58530@gmail.com>
2023-06-14 00:38:16 +0800

    更新

--- Assets/_Scripts/Manager/StartManager.cs
+++ Assets/_Scripts/Manager/StartManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 using Fusion;
 public class StartManager : NetworkBehaviour
 {
+    [SerializeField] private bool dontNeed;
     [SerializeField] private GameObject startCanvas;
     private float time = 3;
     [Networked]
     private TickTimer timer { get; set; }
     public override void Spawned()
     {
+        if (dontNeed)
+        {
+            GameManager.isGame = true;
+            return;
+        }
+
         GameManager.isGame = false;
 
         Runner.Spawn(startCanvas, transform.position, transform.rotation, Object.InputAuthority);
         timer = TickTimer.CreateFromSeconds(Runner, time);
+
     }
     public override void FixedUpdateNetwork()
     {
+        if (dontNeed) return;
+
         if (timer.Expired(Runner))
         {
             GameManager.isGame = true;
